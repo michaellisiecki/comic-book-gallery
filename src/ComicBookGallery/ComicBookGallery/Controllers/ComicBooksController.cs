c611f0d90025278cc7bede7c967e6e374dd7e538
Michał Lisiecki <kendziorr@gmail.com>
2020-05-07 20:43:01 +0200

    Updated Detail action method to use action result types

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,17 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+                return Redirect("/");
+
+            }
+
+            return Content("Hello from the inside!");
+
+
+        }
     }
 }
